62857f36840e0353e8494650dd219a81829aca54
Salomao <salomao.machado.75@gmail.com>
2023-06-20 23:40:14 -0300

    <feat> Classe calculadora de descontos: Criação de classe que fara o calculo de desconto para cada orçamento

---
+++ desconto/CalculadoraDeDescontos.java
@@ -0,0 +1,17 @@
+package loja.desconto;
+
+import java.math.BigDecimal;
+
+import loja.orcamento.Orcamento;
+
+public class CalculadoraDeDescontos {
+	public BigDecimal calcular(Orcamento orcamento) {
+		if(orcamento.getQuantidadeItens() > 5) {
+			return orcamento.getValor().multiply(new BigDecimal("0.1"));
+		}
+		if(orcamento.getValor().compareTo(new BigDecimal("500")) > 0) {
+			return orcamento.getValor().multiply(new BigDecimal("0.1"));
+		}
+		return BigDecimal.ZERO;
+	}
+}
