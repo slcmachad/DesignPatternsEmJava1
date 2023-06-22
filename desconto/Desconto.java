2f35296c7b5391f6015ab99ee62a7d35adfc00b0
Salomao <salomao.machado.75@gmail.com>
2023-06-21 23:05:46 -0300

    <feat> Criação de classe: Criação de classe para utilizar sistema de herança.

---
+++ desconto/Desconto.java
@@ -0,0 +1,16 @@
+package loja.desconto;
+
+import java.math.BigDecimal;
+
+import loja.orcamento.Orcamento;
+
+public abstract class Desconto {
+	
+	protected Desconto proximo;
+	
+	public Desconto(Desconto proximo) {
+		this.proximo = proximo;
+	}
+	
+	public abstract BigDecimal calcular(Orcamento orcamento);
+}
