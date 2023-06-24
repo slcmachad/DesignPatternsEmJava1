15f86d7dd873811b0b8bc64becbf668dc4710658
Salomao <salomao.machado.75@gmail.com>
2023-06-23 23:29:30 -0300

    <feat> divisão de tarefas: criação de classe para retirar metodo e dividir ações dentro da class handler

---
+++ pedido/acao/EnviarEmail.java
@@ -0,0 +1,10 @@
+package loja.pedido.acao;
+
+import loja.pedido.Pedido;
+
+public class EnviarEmail {
+	
+	public void executar(Pedido pedido) {
+		System.out.println("enviando email com dados do pedido");
+	}
+}
---
+++ pedido/acao/SalvarNoBancoDeDados.java
@@ -0,0 +1,10 @@
+package loja.pedido.acao;
+
+import loja.pedido.Pedido;
+
+public class SalvarNoBancoDeDados {
+	
+	public void executar(Pedido pedido) {
+		System.out.println("Salvando pedido no banco de dados");
+	}
+}
