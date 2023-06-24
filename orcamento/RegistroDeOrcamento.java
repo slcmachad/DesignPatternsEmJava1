7b6080d8b659ec999eb085f8865ef4f55413c2c3
Salomao <salomao.machado.75@gmail.com>
2023-06-24 00:24:39 -0300

    <feat> metodo registrar: criação de metodo e extrutura para fazer chamada http para o adaptador a partir do metodo post.

--- orcamento/RegistroDeOrcamento.java
+++ orcamento/RegistroDeOrcamento.java
@@ -1,5 +1,23 @@
 package loja.orcamento;
 
-public class RegistroDeOrcamento {
+import java.util.Map;
 
+import loja.http.HttpAdapter;
+
+public class RegistroDeOrcamento {
+	
+	private HttpAdapter http;
+
+	public RegistroDeOrcamento(HttpAdapter http) {
+		this.http = http;
+	}
+
+	public void registrar(Orcamento orcamento) {
+		String url = "http://api.externa/orcamento";
+		Map<String, Object> dados= Map.of(
+		            "Valor", orcamento.getValor(),
+		            "quantidadeItens", orcamento.getQuantidadeItens()
+		            );
+		http.post(url, dados);
+	}
 }
