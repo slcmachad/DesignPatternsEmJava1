c19e8ee640366a700c2c791cd441b8916b94d03c
Salomao <salomao.machado.75@gmail.com>
2023-06-23 23:15:32 -0300

    <feat> implementação handler: utilização de metodo no qual recebe a injeção de dependencias refatoração para diferenciar injeção de dados da logica.

--- pedido/GeraPedidoHandler.java
+++ pedido/GeraPedidoHandler.java
@@ -1,5 +1,16 @@
 package loja.pedido;
 
-public class GeraPedidoHandler {
+import java.time.LocalDateTime;
 
+import loja.orcamento.Orcamento;
+
+public class GeraPedidoHandler {
+	
+	public void execute(GeraPedido dados) {
+		Orcamento orcamento = new Orcamento(dados.getValorOrcamento(), dados.getQuantidadeDeItens());
+		
+		Pedido pedido = new Pedido(dados.getCliente(), LocalDateTime.now(), orcamento);
+		
+		System.out.println("Salvar pedido no banco de dados");
+	}
 }
