c192b626363284fa1ade1efa13fb4c243cfc80ef
Salomao <salomao.machado.75@gmail.com>
2023-06-23 22:49:51 -0300

    <feat> logica, construtor: criação de construtor par gerar o pedido; criação de logica para executar o construtor a partir dos dados inseridos pelo usuario.

--- pedido/GeraPedido.java
+++ pedido/GeraPedido.java
@@ -1,11 +1,29 @@
 package loja.pedido;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import loja.orcamento.Orcamento;
 
 public class GeraPedido {
 
 	private String cliente;
 	private BigDecimal valorOrcamento;
 	private int quantidadeDeItens;
+	
+	public GeraPedido(String cliente, BigDecimal valorOrcamento, int quantidadeDeItens) {
+		super();
+		this.cliente = cliente;
+		this.valorOrcamento = valorOrcamento;
+		this.quantidadeDeItens = quantidadeDeItens;
+	}
+	
+	public void executa() {
+		Orcamento orcamento = new Orcamento(this.valorOrcamento, this.quantidadeDeItens);
+		
+		Pedido pedido = new Pedido(cliente, LocalDateTime.now(), orcamento);
+		
+		System.out.println("Salvar pedido no banco de dados");
+	}
 	
 }
