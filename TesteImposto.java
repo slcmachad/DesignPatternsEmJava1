1eaa873fea64df083c8c2b0c4a28d5dd17ef5603
Salomao <salomao.machado.75@gmail.com>
2023-06-24 01:24:46 -0300

    <feat> refatoração: complementando codigo para implementar e utilizar classe imposto.

--- TesteImposto.java
+++ TesteImposto.java
@@ -3,14 +3,15 @@ package loja;
 import java.math.BigDecimal;
 
 import loja.imposto.CalculadoraDeImpostos;
+import loja.imposto.ICMS;
 import loja.imposto.ISS;
 import loja.orcamento.Orcamento;
 
-public class TestesDescontos {
-
+public class TesteImposto {
 	public static void main(String[] args) {
-		Orcamento orcamento = new Orcamento(new BigDecimal("1000"), 5);
+		Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
 		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
-		System.out.println(calculadora.calcular(orcamento, new ISS(null)));
+		
+		System.out.println(calculadora.calcular(orcamento, new ICMS(new ISS(null))));
 	}
 }
