4e909e6ab34464c830b594a460d9dd1175b940b8
Salomao <salomao.machado.75@gmail.com>
2023-06-23 22:32:00 -0300

    <feat> gerando pedido: Criação de classe para gerar o pedido separar apresentação dos dados por comando;

---
+++ pedido/GeraPedido.java
@@ -0,0 +1,11 @@
+package loja.pedido;
+
+import java.math.BigDecimal;
+
+public class GeraPedido {
+
+	private String cliente;
+	private BigDecimal valorOrcamento;
+	private int quantidadeDeItens;
+	
+}
